fd28eddaec41c5b0689373d7aeeb7b7bcd8d354a
shinji.nagasaki <shinji.nagasaki@infcurion.com>
2021-02-09 02:24:39 +0900

    add qiita api

--- MyQiita/MainPage.xaml.cs
+++ MyQiita/MainPage.xaml.cs
@@ -5,14 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using MyQiita.Models;
+using MyQiita.Service;
 
 namespace MyQiita
 {
     public partial class MainPage : ContentPage
     {
+        public const string QiitaEndpoint = "https://qiita.com/api/v2/items?page=1&per_page=20";
+        RestService _restService;
+
         public MainPage()
         {
             InitializeComponent();
+            _restService = new RestService();
+
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //Item Views
+            List<QiitaItem> items = await _restService.GetQiitaItemsAsync(QiitaEndpoint);
+            ItemView.ItemsSource = items;
+
         }
     }
 }
---
+++ MyQiita/Service/RestService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MyQiita.Models;
+
+namespace MyQiita.Service
+{
+    public class RestService
+    {
+        HttpClient _client;
+
+        public RestService()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<List<QiitaItem>> GetQiitaItemsAsync(string uri)
+        {
+            List<QiitaItem> items = null;
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    items = JsonConvert.DeserializeObject<List<QiitaItem>>(content);
+                }
+            }catch(Exception ex)
+            {
+                Debug.WriteLine("\tERROR {0}", ex.Message);
+            }
+
+            return items;
+        }
+    }
+}
