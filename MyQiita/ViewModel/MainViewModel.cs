12d97769bc6ffb69288f66b0a4e5039f0af62811
shinji.nagasaki <shinji.nagasaki@infcurion.com>
2021-03-04 02:20:56 +0900

    resolve add ViewModelLocater

--- MyQiita/ViewModel/MainViewModel.cs
+++ MyQiita/ViewModel/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Input;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using Prism.Events;
 using Prism.Commands;
 using Prism.Navigation;
@@ -16,21 +17,18 @@ namespace MyQiita.ViewModel
         private const string QiitaEndpoint = "https://qiita.com/api/v2/items?page=1&per_page=20";
 
         // Property
-        private List<QiitaItem> qiitaItems;
+        private List<QiitaItem> _qiitaItems;
         public List<QiitaItem> QiitaItems
         {
-            get { return qiitaItems; }
-            set { SetProperty<List<QiitaItem>>(ref qiitaItems, value); }
+            get => _qiitaItems;
+            set => SetProperty<List<QiitaItem>>(ref this._qiitaItems, value);
         }
-
-        public ReactiveProperty<string> test { get; } = new ReactiveProperty<string>();
     
         // Constructor
         public MainViewModel(INavigationService navigationService) : base(navigationService)
         {
-            Title.Value = "My Qiita";
+            Title = "My Qiita";
 
-            Title.Subscribe(x => );
             SetQiitaItems();
             ItemSelectCommand = new DelegateCommand(OnItemSelectCommand);
         }
--- MyQiita/ViewModel/ViewModelBase.cs
+++ MyQiita/ViewModel/ViewModelBase.cs
@@ -1,21 +1,44 @@
-﻿using System;
-using System.ComponentModel;
-using Prism.Mvvm;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Prism.Navigation;
-using Reactive.Bindings;
+
 
 namespace MyQiita.ViewModel
 {
-    public class ViewModelBase : BindableBase, INavigationAware, IInitialize, IDestructible, INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged, INavigationAware, IInitialize, IDestructible
     {
         protected INavigationService _navigationService;
+        public event PropertyChangedEventHandler PropertyChanged;
+
 
         public ViewModelBase(INavigationService navigationService)
         {
             _navigationService = navigationService;
         }
 
-        public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>();
+        private string _title;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                this.SetProperty<string>(ref this._title, value);
+            }
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public virtual void OnNavigatedFrom(INavigationParameters parameters) { }
         public virtual void OnNavigatedTo(INavigationParameters parameters) { }
